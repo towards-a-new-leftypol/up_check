d40155fcdad2b62e162b43e8d125556b7875c385
towards-a-new-leftypol <paul_cockshott@protonmail.com>
2026-03-01 23:55:49 -0500

    init

---
+++ default.nix
@@ -0,0 +1,20 @@
+{ pkgs ? import <nixpkgs> {}, }:
+
+let
+  haskell = pkgs.haskell.packages.ghc;
+
+  drv = haskell.callCabal2nix "up-check" ./. {
+  };
+
+  env = drv.env.overrideAttrs (oldAttrs: {
+    buildInputs = oldAttrs.buildInputs ++ [
+      pkgs.haskellPackages.cabal-install
+      haskell.miso-from-html
+      haskell.hlint
+      haskell.haskell-language-server
+    ];
+  });
+
+in
+
+if pkgs.lib.inNixShell then env else drv
