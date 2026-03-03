b3ea643f00c13252216e06e2f7e70ebc2d20418c
towards-a-new-leftypol <paul_cockshott@protonmail.com>
2026-03-02 19:08:21 -0500

    it builds but socks.hs was entirely vibeslopped

--- src-exe/Main.hs
+++ src-exe/Main.hs
@@ -1,12 +1,71 @@
+{-# LANGUAGE DeriveDataTypeable #-}
+{-# LANGUAGE DeriveAnyClass #-}
+{-# LANGUAGE DeriveGeneric #-}
+
 module Main where
 
-import HttpClient
+import qualified Data.ByteString.Lazy as B
+import System.Console.CmdArgs (cmdArgs, Data)
+import System.Exit (exitFailure)
+import Data.Aeson (decode, encode, FromJSON, ToJSON)
+import GHC.Generics
+import Control.Monad.Trans.Except (ExceptT (..), runExceptT)
+import Data.Bifunctor (first)
+
+import HttpClient (get, HttpError)
+
+
+newtype CliArgs = CliArgs
+  { settingsFile :: String
+  } deriving (Show, Data)
+
+
+data JSONSettings = JSONSettings
+    { get_urls :: [ String ]
+    } deriving (Generic, FromJSON, ToJSON)
+
+
+data ProgramException = HttpException HttpError
+  deriving Show
+
+
+type IOe a = ExceptT ProgramException IO a
+
+liftHttpIO :: IO (Either HttpError a) -> IOe a
+liftHttpIO = ExceptT . fmap (first HttpException)
+
+getSettings :: IO JSONSettings
+getSettings = do
+    cliArgs <- cmdArgs $ CliArgs "settings.json"
+
+    let filePath = settingsFile cliArgs
+    if null filePath
+    then do
+        putStrLn "Error: No JSON settings file provided."
+        exitFailure
+    else do
+        putStrLn $ "Loading settings from: " ++ filePath
+        content <- B.readFile filePath
+        case decode content :: Maybe JSONSettings of
+            Nothing -> do
+                putStrLn "Error: Invalid JSON format."
+                exitFailure
+            Just settings -> return settings
 
 
 main :: IO ()
 main = do
     putStrLn "Hello, Haskell!"
 
-    print =<< get "https://google.com" []
+    settings <- getSettings
 
-    putStrLn "Bye!"
+    print $ encode settings
+
+    endResult <- runExceptT $
+        mapM (\u -> liftHttpIO $ get u []) (get_urls settings)
+
+    case endResult of
+        Left e -> do
+            print e
+            exitFailure
+        Right _ -> putStrLn "Success"
---
+++ src-exe/Socks.hs
@@ -0,0 +1,69 @@
+{-# LANGUAGE OverloadedStrings #-}
+
+module Socks
+  ( mkSocksManager
+  ) where
+
+import Network.HTTP.Client
+    ( Manager
+    , newManager
+    , managerRawConnection
+    , managerTlsConnection
+    , socketConnection
+    , managerSetMaxHeaderLength
+    )
+import Network.HTTP.Client.Internal (Connection)
+import Network.HTTP.Client.TLS (tlsManagerSettings)
+import Network.Socks5
+    ( SocksConf(..)
+    , SocksAddress(..)
+    , SocksHostAddress(..)
+    , defaultSocksConf
+    , socksConnect
+    )
+import Network.Socks5.Types
+  ( SocksVersion(..)
+  )
+import Network.Socket
+    ( SockAddr(..)
+    , getAddrInfo
+    , defaultHints
+    , addrAddress
+    , addrSocketType
+    , SocketType(..)
+    , withSocketsDo
+    , HostAddress
+    )
+import qualified Data.List.NonEmpty as NE
+import qualified Data.ByteString.Char8 as BS8
+
+mkSocksManager :: String -> Int -> IO Manager
+mkSocksManager proxyHost proxyPort = withSocketsDo $ do
+    proxyAddr <- resolveSockAddr proxyHost proxyPort
+    
+    let socksConf = (defaultSocksConf proxyAddr)
+            { socksVersion = SocksVer5
+            }
+    
+    let makeSocksConnection :: Maybe HostAddress -> String -> Int -> IO Connection
+        makeSocksConnection _maybeLocalIP destHost destPort = do
+            let destAddr = SocksAddress 
+                    (SocksAddrDomainName (BS8.pack destHost)) 
+                    (fromIntegral destPort)
+            
+            (sock, _resolved) <- socksConnect socksConf destAddr
+            socketConnection sock 4096
+    
+    newManager $ managerSetMaxHeaderLength (16384 * 4)
+        (tlsManagerSettings 
+            { managerRawConnection = return makeSocksConnection
+            , managerTlsConnection = return makeSocksConnection
+            })
+
+resolveSockAddr :: String -> Int -> IO SockAddr
+resolveSockAddr host port = do
+    let hints = defaultHints { addrSocketType = Stream }
+    addrs <- getAddrInfo (Just hints) (Just host) (Just (show port))
+    case NE.nonEmpty addrs of
+        Nothing -> fail $ "Could not resolve SOCKS proxy address: " ++ host
+        Just nonEmpty -> return $ addrAddress $ NE.head nonEmpty
